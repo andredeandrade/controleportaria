ff82873dd1b885c143473710966cb41748d58d96
André <andre@andrade.inf.br>
2026-06-13 12:18:29 -0300

    feat: add module service providers

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from 'express'
 import { authRouter } from '../modules/auth/index.js'
 import { condominiumsRouter } from '../modules/condominiums/index.js'
 import { residentsRouter } from '../modules/residents/index.js'
+import { serviceProvidersRouter } from '../modules/service-providers/index.js'
 import { visitorsRouter } from '../modules/visitors/index.js'
 
 export const router = Router()
@@ -10,3 +11,4 @@ router.use('/condominiums', condominiumsRouter)
 router.use('/auth', authRouter)
 router.use('/residents', residentsRouter)
 router.use('/visitors', visitorsRouter)
+router.use('/service-providers', serviceProvidersRouter)
